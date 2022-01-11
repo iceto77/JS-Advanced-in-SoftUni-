d7a4363040cbeb643c3fd043525a1f562d431e12
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2022-01-11 21:59:35 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements - Lab/04.CircleArea/circleArea.js
@@ -0,0 +1,12 @@
+function slove(input){
+    let inputType = typeof(input);
+    if (inputType === 'number'){
+        let result = Math.PI * Math.pow(input, 2);
+        console.log(result.toFixed(2))
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+    }
+}
+
+slove(5);
+slove('name');
